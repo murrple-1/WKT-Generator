f312609db5f5ee0ac0d065b8458f844baedfa722
Murray Christopherson <mchristopherson@tradewind.aero>
2020-12-31 16:56:16 -0800

    setup how I like it

--- wkt-generator/src/app/app.component.ts
+++ wkt-generator/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+
+import { Subject } from 'rxjs';
+
+import { MapOptions, tileLayer, latLng, Map } from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'wkt-generator';
+export class AppComponent implements OnDestroy {
+  leafletOptions: MapOptions = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+      }),
+    ],
+    zoom: 4,
+    maxZoom: 18,
+    center: latLng(58.784771, -110.009441),
+  };
+
+  private map: Map | null = null;
+
+  private unsubscribe$ = new Subject<void>();
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  onMapReady(map: Map) {
+    this.map = map;
+  }
 }
--- wkt-generator/src/app/app.module.ts
+++ wkt-generator/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, LeafletModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
